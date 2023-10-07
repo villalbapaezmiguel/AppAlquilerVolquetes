d2788f3e4fe2f0daacc17d868d4eee01831408da
villalbapaezmiguel <villalbapaezmiguel@gmai.com>
2023-10-06 23:26:34 -0300

    Comenzamos a refactorizar todo el codigo

---
+++ Entidades/EntidadesControl/UsuarioControl.cs
@@ -0,0 +1,92 @@
+﻿using Entidades.EntidadesUsuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.EntidadesControl
+{
+    public sealed class UsuarioControl 
+    {
+        private static List<Usuario> listaUsuarios = new List<Usuario>();
+        private static Usuario usuarioActual;
+        
+        public static bool AgregarUsuario(Usuario nuevoUsurio)
+        {
+            if(nuevoUsurio is not null)
+            {
+                if(!ExisteUsuario(nuevoUsurio.NombreUsuario))
+                {
+                    listaUsuarios.Add(nuevoUsurio);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ExisteUsuario( string nombreUsuario)
+        {
+            bool exite = listaUsuarios.Any(usuario => usuario.NombreUsuario == nombreUsuario);
+            return exite;
+        }
+
+        public static Usuario GetUsuario
+        {
+            get
+            {
+                return usuarioActual;
+            }
+        }
+        public static Usuario SetUsuario
+        {
+            set
+            {
+                usuarioActual = value;
+            }
+        }
+
+
+        public static List<Usuario> GetListaUsuarios
+        {
+            get
+            {
+                List<Usuario> nuevaLista = new();
+                foreach(Usuario usuario in listaUsuarios) 
+                {
+                    Usuario AuxUsuario = new(usuario.NombreUsuario, usuario.Clave, usuario.Telefono, usuario.Nombre, usuario.Apellido, usuario.Dni);
+                    nuevaLista.Add(AuxUsuario);
+                }
+                return nuevaLista;
+            }
+        }
+
+
+        public static Usuario? BuscarUsuarioPorClaveYNombreUsuario(string nombreUsuario,  string clave)
+        {
+            if(nombreUsuario != string.Empty && clave != string.Empty)
+            {
+                if(ExisteUsuario(nombreUsuario)) 
+                {
+                    foreach(Usuario auxItem in GetListaUsuarios)
+                    {
+                        if(auxItem.NombreUsuario == nombreUsuario && auxItem.Clave == clave) 
+                        {
+                            return auxItem;
+                        }
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+
+
+
+
+    }
+}
---
+++ Entidades/EntidadesControl/VolqueteControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.EntidadesControl
+{
+    public sealed class VolqueteControl
+    {
+        private static List<Volquete> listaVolquetes = new List<Volquete>();
+
+
+        public static bool AgregarVolquete(Volquete volquete)
+        {
+            if(volquete is not null)
+            {
+                if(!ExistePorElMarca(volquete.Marca))
+                {
+                    listaVolquetes.Add(volquete);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ExistePorElMarca(string marca)
+        {
+            bool existe = listaVolquetes.Exists(volquete => volquete.Marca == marca);
+            return existe;
+        }
+
+
+
+        /// <summary>
+        /// Devuelvo la lista de volquetes Sin la referencia actual de la lista original de volquetes
+        /// </summary>
+        public List<Volquete> GetListaVolquetes
+        {
+            get
+            {
+                List<Volquete> nuevaLista = new();
+                foreach(Volquete item in listaVolquetes) 
+                { 
+                    Volquete nuevo = new Volquete(item.Marca , item.Altura, item.Largo,item.Ancho,item.Precio,item.DiasDeArriendo);
+                    nuevaLista.Add(nuevo);
+                }            
+                return nuevaLista;
+            }
+        }
+
+
+    }
+}
