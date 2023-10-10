b5b6c717835a6bf203da0805ccdca47125dceace
villalbapaezmiguel <villalbapaezmiguel@gmai.com>
2023-10-10 01:31:34 -0300

    Nuevos cambios jee

---
+++ Entidades/EntidadesControl/CompraControl.cs
@@ -0,0 +1,56 @@
+﻿using Entidades.EntidadesUsuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.EntidadesControl
+{
+    public sealed class CompraControl
+    {
+        private static List<Compra> listaCompras = new List<Compra>();
+        private static int id = 0;
+
+        private static int NuevoId()
+        {
+            return id ++;
+        }
+        public static bool AgregarCompra(ref Compra compra)
+        {
+            if (compra is not null)
+            {
+                if (!ExisteCompra(compra.IdCompra))//problemas con los id , hay que solucionarlo
+                {
+                    listaCompras.Add(compra);
+                    compra.IdCompra = NuevoId();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ExisteCompra(int idCompra)
+        {
+            bool existe = listaCompras.Any(compra => compra.IdCompra == idCompra);
+            return existe;
+        }
+        public static List<Compra> GetListaCompras
+        {
+            get
+            {
+                List<Compra> nuevaLista = new();
+              
+                foreach (Compra item in listaCompras)
+                {
+                    Compra AuxUsuario = new(item.TipoVolquete , item.NombreDeUsuario,item.CantidadVolquetes,item.CantidadDias,item.FechaDeEntraga,item.HoraDeEntrega,item.Direccion,item.Precio);
+                    nuevaLista.Add(AuxUsuario);
+                }
+                return nuevaLista;
+            }
+        }
+
+
+    }
+}
---
+++ Entidades/EntidadesUsuarios/Compra.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.EntidadesUsuarios
+{
+    public class Compra
+    {
+        private string tipoVolquete;
+        private string nombreDeUsuario;
+        private int cantidadVolquetes;
+        private int cantidadDias;
+        private DateTime fechaDeEntraga;
+        private string horaDeEntrega;
+        private string direccion;
+        private float precio;
+        private  int idCompra = 0;
+        public Compra()
+        {
+
+        }
+        public Compra(string volquete, string nombreDeUsuario, int cantidadVolquetes, int cantidadDias, DateTime fechaDeEntraga, string horaDeEntrega, string direccion, float precio) : this()
+        {
+            this.tipoVolquete = volquete;
+            this.nombreDeUsuario = nombreDeUsuario;
+            this.cantidadVolquetes = cantidadVolquetes;
+            this.cantidadDias = cantidadDias;
+            this.fechaDeEntraga = fechaDeEntraga;
+            this.horaDeEntrega = horaDeEntrega;
+            this.direccion = direccion;
+            this.precio = precio;
+        }
+
+        public string NombreDeUsuario { get => nombreDeUsuario; set => nombreDeUsuario = value; }
+        public int CantidadVolquetes { get => cantidadVolquetes; set => cantidadVolquetes = value; }
+        public int CantidadDias { get => cantidadDias; set => cantidadDias = value; }
+        public DateTime FechaDeEntraga { get => fechaDeEntraga; set => fechaDeEntraga = value; }
+        public string HoraDeEntrega { get => horaDeEntrega; set => horaDeEntrega = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public float Precio { get => precio; set => precio = value; }
+        public string TipoVolquete { get => tipoVolquete; set => tipoVolquete = value; }
+        public int IdCompra { get => idCompra; set => idCompra = value; }
+
+
+
+    }
+}
